efe6e7e8a95ef213349d3215b7b62deb3e4ee134
Manssizz <fajarmanssizz@gmail.com>
2024-11-23 11:27:24 +0700

    remove duplicate

--- make-openwrt/openwrt-files/common-files/usr/lib/lua/luci/controller/modem-reconnect.lua
+++
@@ -1,5 +0,0 @@
-module ("luci.controller.modem-reconnect", package.seeall)
-
-function index()
-    entry({"admin", "modem", "modem-reconnect"}, cbi("modem-reconenct"), _("Modem Reconnect"), 60).dependent = true
-end
