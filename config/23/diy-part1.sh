490b2563b677d4cc9fccd0ec63ecdafb75763244
manssizz <fajarmanssizz@gmail.com>
2025-07-28 21:11:01 +0700

    update

--- config/23/diy-part1.sh
+++ config/23/diy-part1.sh
@@ -9,7 +9,9 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-sed -i '$a src-git modemfeed https://github.com/koshev-msk/modemfeed.git' feeds.conf.default
+# sed -i '$a src-git modemfeed https://github.com/koshev-msk/modemfeed.git' feeds.conf.default
+sed -i '$a src-git at https://github.com/4IceG/luci-app-atinout-mod.git' feeds.conf.default
+sed -i '$a src-git xmm https://github.com/lutfailham96/xmm-modem.git' feeds.conf.default
 sed -i '$a src-git additional https://github.com/NueXini/NueXini_Packages.git;main' feeds.conf.default
 sed -i '$a src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' feeds.conf.default
 # sed -i '$a src-git filebrowser https://github.com/xiaozhuai/luci-app-filebrowser.git' feeds.conf.default
